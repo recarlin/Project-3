8cb439fbd3d3c0e4f2c71b45b2f383a53a42965f
Russell Carlin <recarlin@fullsail.com>
2011-12-07 16:55:03 -0500

    Finished the material picker
    At least I think so; all it needs is the call at the end.

--- Carlin_Russell_Project3.js
+++ Carlin_Russell_Project3.js
@@ -1,8 +1,25 @@
 var phrase = "SDI IS SWEET",
     friendsOnline = true
+
 var getStarted = function (spellOut) {
     console.log("I think my Minecraft world needs some decoration. How about giant letters spelling out " + spellOut + "! Yeah, that is just what this place needs!")
-        if (friendsOnline == true) {}
+        if (friendsOnline == true) {
+            console.log("Some friends are on, maybe I could get their help. But first I need to figure out what I'm going to make it out of.")
+        } else {
+            console.log("Seems everyone is offline... I guess I'll have to do this all on my own. Well, what should I make it out of?")
+        };
+};
+
+var getMaterial = function () {
+    var materials = ["cobblestone", "wood", "wool"]
+        matPick = [Math.floor(Math.random()*materials.length)]
+    console.log("I think " + matPick + " would look good.")
+        if (friendsOnline == true) {
+            console.log("I'll need to gather the " + matPick + " but I wonder if anyone has any I could borrow?")
+        } else {
+            console.log("Everyone is offline, so I guess I'll have to harvest the " + matPick + " without help.")
+        };
+    return matPick
 };
 
 var getBlocks = function (letter) {
