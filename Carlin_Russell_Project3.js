72a340a974715b6c6aff5783ceb4f13c9019ab15
Russell Carlin <recarlin@fullsail.com>
2011-12-07 21:49:08 -0500

    Making a branch
    Fixed getMaterials and making a branch to try something.

--- Carlin_Russell_Project3.js
+++ Carlin_Russell_Project3.js
@@ -12,10 +12,10 @@ var getStarted = function (spellOut) {
 
 var getMaterial = function () {
     var materials = ["cobblestone", "wood", "wool"]
-        matPick = [Math.floor(Math.random()*materials.length)]
+        matPick = materials[Math.floor(Math.random()*materials.length)]
     console.log("I think " + matPick + " would look good.")
         if (friendsOnline == true) {
-            console.log("I'll need to gather the " + matPick + " but I wonder if anyone has any I could borrow?")
+            console.log("I could go gather " + matPick + " but I wonder if anyone would lend it to me?")
         } else {
             console.log("Everyone is offline, so I guess I'll have to harvest the " + matPick + " without help.")
         };
@@ -59,13 +59,32 @@ var getBlocks = function (letter) {
 var getResources = function (matPick) {
     var letters = phrase.split(""),
         need = 0
-    console.log(letters)
         for (i = 0, l = letters.length; i < l; i++) {
             need = need + getBlocks(letters[i]);
-        console.log(need)
         };
+        if (matPick == "wood") {
+            need = need / 4
+            console.log("Since I'm using wood and I get 4 wood from each log, I should only need about " + need + " logs to finish the job.")
+        } else {
+            console.log("I will need about " + need + " " + matPick + " blocks to finish the job.")
+        };
+        if (friendsOnline == true) {
+            console.log("My buddies are on, I'll ask them if they have the stuff I need.")
+            console.log("Hey guys, can I borrow " + need + " " + matPick + " for something I want to build?")
+            var answer = Math.round(Math.random());
+                if (answer == 0) {
+                    console.log("Sure. It is all in the box here.")
+                    console.log("Thanks! I'll be sure to return the favor.")
+                } else {
+                    console.log("Sorry, I need my " + matPick + " for a project I am doing.")
+                    console.log("Ok, I guess I'll go collect it then, thanks anyway.")
+                };
+        } else {
+            console.log("Everyone is offline, so I'll have to go collect the " + matPick + " myself. Better get started!")
+        };
+        console.log("Ok, now that I got the " + matPick + " that I need, time to build!")
     return need;
 };
 
-need = getResources()
-// console.log(need)
+matPick = getMaterial()
+need = getResources(matPick)
