4947345f21d76b952145ae1643611e8302fc69ea
Russell Carlin <recarlin@fullsail.com>
2011-12-08 17:21:03 -0500

    FINISHED!
    Added the final doIt function and I think I'm finished.

--- Carlin_Russell_Project3.js
+++ Carlin_Russell_Project3.js
@@ -1,7 +1,7 @@
 var phrase = "CODING IS FUN",
     friendsOnline = true;
-var getStarted = function (spellOut) {
-    console.log("I think my Minecraft world needs some decoration. How about giant letters spelling out " + spellOut + "! Yeah, that is just what this place needs!");
+var getStarted = function (phrase) {
+    console.log("I think my Minecraft world needs some decoration. How about giant letters spelling out " + phrase + "! Yeah, that is just what this place needs!");
         if (friendsOnline == true) {
             console.log("Some friends are on, maybe I could get their help. But first I need to figure out what I'm going to make it out of.");
         } else {
@@ -71,53 +71,91 @@ var getResources = function (matPick) {
     return need;
 };
 var getFriend = function (name) {
-        var friend = {
-            name: name,
-            tool: {},
-            ask: function () {
-                var yesNo = Math.round(Math.random)
-                if (yesNo == 0) {
-                    return true
-                } else {
-                    return false
-                }
-            },
-            getTool: function () {  
-                var getQuality = function () {
-                    var m = ["diamond", "gold", "iron"]
-                    q = m[Math.floor(Math.random()*m.length)]
-                    return q;
-                };
-                var getType = function () {
-                    var t = ["sheers", "shovel", "pick"]
-                    i = t[Math.floor(Math.random()*t.length)]
-                    return i;
-                };
-                var t = {
-                    type: getType(),
-                    quality: getQuality(),
-                    durability: Math.ceil(Math.random()*100)
-                };
-                return t;
+    var friend = {
+        name: "",
+        tool: {},
+        ask: function () {
+            var yesNo = Math.round(Math.random())
+            if (yesNo == 0) {
+                return true
+            } else {
+                return false
             }
-        };
+        },
+        getTool: function () {  
+            var getQuality = function () {
+                var m = ["diamond", "gold", "iron"]
+                q = m[Math.floor(Math.random()*m.length)]
+                return q;
+            };
+            var getType = function () {
+                var t = ["sheers", "shovel", "pick"]
+                    i = t[Math.floor(Math.random()*t.length)]
+                return i;
+            };
+            var t = {
+                type: getType(),
+                quality: getQuality(),
+                durability: Math.ceil(Math.random()*100)
+            };
+            friend.tool = t;
+        },
+        getName: function () {
+            var d = ["Johnny", "Paul", "Phil"]
+                n = d[Math.floor(Math.random()*d.length)]
+            return n;
+        }
+    };
     return friend
 };
 var buildLetters = function (matPick, need) {
     var words = phrase.split(" ")
         wordOn = 0
-        letters = words[wordOn].split("")
-        console.log(words)
     while (wordOn !== words.length) {
+        letters = words[wordOn].split("")
         for (i = 0, l = letters.length; i < l; i++) {
             console.log("Got the " + letters[i] + " done.")
         }
         console.log("Got the " + words[wordOn] + " done.")
         wordOn++
-    }
+    };
 };
-matPick = getMaterial()
-bob = getFriend("Bob")
-bob.tool = bob.getTool()
-need = getResources(matPick)
-buildLetters(matPick, need)
+
+
+var doIt = function (){
+    getStarted(phrase)
+    matPick = getMaterial()
+    need = getResources(matPick);
+        if (friendsOnline == true) {
+            friend = getFriend();
+            friendName = friend.getName()
+                console.log("I think I will ask my friend " + friendName + " if they can lend me the resources.");
+                console.log("Hey " + friendName + ", can I borrow some " + matPick + " to make something?")
+            answer = friend.ask()
+                if (answer == true) {
+                    console.log("Sure.")
+                    console.log("Thanks a lot!")
+                } else {
+                    console.log("I need my " + matPick + " for a project. Sorry!")
+                    console.log("That's fine. Thanks anyway.")
+                    console.log("Guess I'll need to get the stuff myself.")
+                    friend.getTool()
+                    answer = friend.ask()
+                    console.log("Could I borrow a tool?")
+                        if (answer == true) {
+                            console.log("All I have is a " + friend.tool.quality + " " + friend.tool.type + " with " + friend.tool.durability + " durabilty, but sure.")
+                            console.log("Thanks!")
+                        } else {
+                            console.log("Sorry, I'm using my " + friend.tool.quality + " " + friend.tool.type + ".")
+                            console.log("That's fine, I'll go ahead and grab everything. See ya later.")
+                        };
+                };   
+        } else {
+            console.log("No one is on to get help from. Oh well, I'll go get the stuff myself.")
+        };
+    console.log("I got all of my materials, time to build!")
+        buildLetters()
+    console.log("AWESOME! I got " + phrase + " spelled out in big, " + matPick + " letters!")
+};
+
+doIt()
